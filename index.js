866d9b2a6f253ff0e9f078574c4194a88e5cc35d
Xzandro <mail@xzandro.de>
2018-07-31 05:45:38 +0200

    Changed fucntion name Added check if mapKey is available Added some environment variables

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const parseString = require("xml2js").parseString;
 const fs = require("fs-extra");
 const path = require("path");
 
-const mapKey = "sku";
-const fileDir = "./files";
-const outputDir = "./output";
+const mapKey = process.env.MAP_KEY || "sku";
+const fileDir = process.env.FILE_DIR || "./files";
+const outputDir = process.env.OUTPUT_DIR || "./output";
 
-// read files folder
-const process = async () => {
+// read files from folder
+const processFiles = async () => {
   const files = await fs.readdir(fileDir);
-  
+
   files.forEach(file => {
     fs.readFile(path.join(fileDir, file)).then(fileData => {
       parseString(fileData, function(err, result) {
         if (result && result.Definitions) {
-          const convertedObject = {};
+          const convertedObject = result.Definitions.Definition[0].$[mapKey] ? {} : [];
           result.Definitions.Definition.forEach(definition => {
-            convertedObject[definition.$[mapKey]] = definition.$;
+            // if mapKey not available just convert the definitions to normal objects and push them to an array
+            if (result.Definitions.Definition[0].$[mapKey]) {
+              convertedObject[definition.$[mapKey]] = definition.$;
+            } else {
+              convertedObject.push(definition.$);
+            }
           });
           fs.outputJson(path.join(outputDir, `${file}.json`), convertedObject).then(() => {
             console.log(`${file}.json created.`);
@@ -27,4 +32,4 @@ const process = async () => {
   });
 };
 
-process();
+processFiles();
